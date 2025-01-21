0d274711e158fc8035a169ba3f7c773ed00e6719
Ankush307 <ankushdhull966@gmail.com>
2025-01-21 12:38:59 +0530

    updates

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -6,21 +6,21 @@ const Header = () => {
 
     return (
         <div className='max-w-[1128px] mt-2.5 max-md:flex-col gap-4 w-full shadow-[0px_10px_10px_0px_#31374D1A] pl-[38.48px] rounded-[20px] items-center py-2.5 pr-3 max-md:p-2 flex justify-between mx-auto'>
-             <Link to="/" className='max-w-svg max-md:hidden'><PageLogo /></Link>
+             <Link to="/" className='max-w-[135px] max-md:hidden'><PageLogo /></Link>
             <div className="flex gap-4 md:hidden items-center justify-center">
                 <Link to="/" className=' md:hidden'><PageLogo /></Link>
                 <button className='border-none outline-none hover:scale-110 transition-all text-sm duration-300'>Login</button>
-                <button className='py-2 px-3 whitespace-nowrap bg-black hover:scale-110 transition-all duration-300 text-white rounded-[9px]'>Sign Up</button>
+                <button className='py-2 px-3 text-sm whitespace-nowrap bg-black hover:scale-110 transition-all duration-300 text-white rounded-[9px]'>Sign Up</button>
             </div>
-            <div className='max-w-[650px] max-sm:max-w-[300px] max-md:max-w-[400px] max-xl:mx-2 w-full p-[6px_7px_6px_30px] max-md:pl-2.5 border border-solid border-sky-blue rounded-[144px]'>
+            <div className='max-w-[650px] max-sm:max-w-[300px] max-md:max-w-[400px] max-xl:mx-2 w-full p-[6px_7px_6px_30px] max-md:pl-2.5 border border-solid border-[#DBDBF3] rounded-[144px]'>
                 <form className='flex w-full justify-between'>
-                    <input required placeholder="What do you feel like listening to right now ?" type="text" className='border-none bg-transparent outline-none w-[85%] max-xl:w-[75%] max-md:text-xs ' />
-                    <button className='p-[11.94px_12.08px_11.08px_10.95px] bg-black rounded-full hover:scale-110 transition-all duration-300 '><SearchIcon /> </button>
+                    <input required placeholder="What do you feel like listening to right now ?" type="text" className='border-none bg-transparent outline-none w-[85%] max-xl:w-[75%] text-[#333333] placeholder:text-[#333333] max-md:text-xs ' />
+                    <button type='submit' className='p-[11px_12px_12px_12px] bg-black rounded-full hover:scale-110 transition-all duration-300 '><SearchIcon /> </button>
                 </form>
             </div>
             <div className="flex gap-5 max-md:hidden">
-                <button className='border-none outline-none hover:scale-110 transition-all duration-300'>Login</button>
-                <button className='pt-[9px] px-6 pb-2.5 whitespace-nowrap bg-black hover:scale-110 transition-all duration-300 text-white rounded-[9px]'>Sign Up</button>
+                <button className='border-none outline-none hover:scale-110 text-sm transition-all duration-300'>Login</button>
+                <button className='pt-[9px] px-6 pb-2.5 whitespace-nowrap bg-black text-sm hover:scale-110 transition-all duration-300 text-white rounded-[9px]'>Sign Up</button>
             </div>
         </div>
     )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,8 @@ const Hero = () => {
     const [alphabetText, setAlphabet] = useState()
     const navigate = useNavigate();
 
-    const handleAlphabetChande = (newText) => (
+    const handleAlphabetChange = (newText) => (
+        
         setAlphabet(`${newText}`)
     )
 
@@ -28,32 +29,32 @@ const Hero = () => {
             <div className="container mx-auto max-w-[1140px] max-xl:px-5">
                 <Header />
                 <div className="flex items-center mt-[17px] max-xl:overflow-x-scroll max-xl:pb-5">
-                    <div className="flex items-center gap-5 me-[15px]">
-                        <button className="min-w-[49px] h-[29px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-darkBlack" onClick={() => handleCategoryClick("All")}> All</button>
-                        <button className="min-w-[47px] h-[29px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-darkBlack" onClick={() => handleCategoryClick("Pop")}> Pop</button>
-                        <button className="h-[29px] min-w-[54px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-darkBlack" onClick={() => handleCategoryClick("Rock")}> Rock</button>
-                        <button className="flex items-center gap-4 justify-center h-[29px] min-w-[64px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 font-normal text-darkBlack" onClick={() => handleCategoryClick("More")}> More <ArrowIcon /></button>
+                    <div className="flex items-center gap-[5px] me-[15px]">
+                        <button className="min-w-[43px] h-[29px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-[#14191C]" onClick={() => handleCategoryClick("All")}> All</button>
+                        <button className="min-w-[47px] h-[29px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-[#14191C]" onClick={() => handleCategoryClick("Pop")}> Pop</button>
+                        <button className="h-[29px] min-w-[54px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-[#14191C]" onClick={() => handleCategoryClick("Rock")}> Rock</button>
+                        <button className="flex items-center gap-[5px] justify-center h-[29px] min-w-[64px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 font-normal text-[#14191C]" onClick={() => handleCategoryClick("More")}> More <ArrowIcon /></button>
                     </div>
                     {ALPHABET_LIST.map((obj, i) => (
-                        <p className="size-[29px] max-xl:overflow-x-scroll min-w-[29px] text-xs hover:text-[#FAFAFF] cursor-pointer font-medium leading-[18px] rounded-full flex items-center justify-center bg-transparent transition-all ease-linear duration-200 hover:bg-black text-darkBlack uppercase" key={i} onClick={() => handleAlphabetChande(obj)}> {obj}</p>
+                        <p className="size-[29px] max-xl:overflow-x-scroll min-w-[29px] text-xs hover:text-[#FAFAFF] cursor-pointer font-medium leading-[18px] rounded-full flex items-center justify-center bg-transparent transition-all ease-linear duration-200 hover:bg-black text-[#14191C]" key={i} onClick={() => handleAlphabetChange(obj)}> {obj}</p>
                     ))}
                 </div>
-                <div className="w-full max-w-[1141px] mx-auto rounded-[20px] bg-dark-black h-[347px] relative mt-[43px] max-lg:h-full max-lg:mt-7">
+                <div className="w-full max-w-[1141px] mx-auto rounded-[20px] bg-[#14191C] h-[347px] relative mt-[43px] max-lg:h-full max-lg:mt-7">
                     <div className="flex justify-between max-lg:flex-wrap">
-                        <div className="flex flex-col md:ps-[48px] pt-[48px] max-xl:ps-8 max-xl:pt-8 max-md:ps-5 max-md:pt-5 max-md:w-full">
-                            <h1 className="text-5xl max-lg:text-4xl max-md:text-3xl font-bold leading-[58.51px] max-md:text-center text-[#FAFAFF] uppercase">{getHeading()}</h1>
-                            <div className="flex items-center gap-6 absolute bottom-[-58px] max-xl:bottom-0 max-lg:hidden">
+                        <div className="flex flex-col lg:ps-[48px] pt-[38px] max-sm:px-3 max-xl:pt-8 max-md:pt-5 max-xl:w-full max-lg:items-center max-lg:justify-center">
+                            <h1 className="text-5xl font-montserrat max-lg:text-4xl max-md:text-3xl max-sm:text-2xl font-bold leading-[58.51px] max-md:text-center text-[#FAFAFF] uppercase">{getHeading()}</h1>
+                            <div className="flex items-center gap-6 absolute xl:bottom-[-58px] bottom-[-44px] max-lg:bottom-0 max-lg:hidden">
                                 <img src="./assets/images/album-dp.webp" alt="user-img" className="cursor-pointer max-xl:size-40" />
                                 <div className="flex flex-col">
                                     <h3 className="text-white text-[32px] font-semibold leading-[42px]"> Billie Eilish {alphabetText} </h3>
-                                    <p className="text-base text-[#CECECE] font-medium leading-[19.5px]">Released May 17, 2024</p>
+                                    <p className="text-base font-montserrat text-[#CECECE] font-medium leading-[19.5px]">Released May 17, 2024</p>
                                 </div>
                             </div>
                             <div className="flex items-center gap-6 lg:hidden mt-5">
                                 <img src="./assets/images/album-dp.webp" alt="user-img" className="cursor-pointer max-xl:size-40 max-lg:size-20 max-md:size-16" />
                                 <div className="flex flex-col">
-                                    <h3 className="text-white text-[32px] font-semibold leading-[42px] max-lg:text-2xl">Billie Eilish {alphabetText}</h3>
-                                    <p className="text-base text-[#CECECE] font-medium leading-[19.5px]"> Released May 17, 2024</p>
+                                    <h3 className="text-white text-[32px] font-semibold leading-[42px] max-lg:text-2xl max-md:text-xl">Billie Eilish {alphabetText}</h3>
+                                    <p className="text-base font-montserrat text-[#CECECE] font-medium leading-[19.5px]"> Released May 17, 2024</p>
                                 </div>
                             </div>
                         </div>
