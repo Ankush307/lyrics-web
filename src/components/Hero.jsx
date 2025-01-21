bb3e5535ebd2a9101a3f7678468d14e4d31f9775
Ankush307 <ankushdhull966@gmail.com>
2025-01-21 14:45:33 +0530

    change hover

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,11 +10,13 @@ const Hero = () => {
     const navigate = useNavigate();
 
     const handleAlphabetChange = (newText) => (
-        
         setAlphabet(`${newText}`)
     )
 
-    const handleCategoryClick = (category) => setMusicCategory(category);
+    const handleCategoryClick = (category) => {
+        setMusicCategory(category);
+        navigate(`?value=${category.toLowerCase()}`);
+    }
 
     const getHeading = () =>
     ({
@@ -30,10 +32,10 @@ const Hero = () => {
                 <Header />
                 <div className="flex items-center mt-[17px] max-xl:overflow-x-scroll max-xl:pb-5">
                     <div className="flex items-center gap-[5px] me-[15px]">
-                        <button className="min-w-[43px] h-[29px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-[#14191C]" onClick={() => handleCategoryClick("All")}> All</button>
-                        <button className="min-w-[47px] h-[29px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-[#14191C]" onClick={() => handleCategoryClick("Pop")}> Pop</button>
-                        <button className="h-[29px] min-w-[54px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 hover:bg-black hover:text-white font-normal text-[#14191C]" onClick={() => handleCategoryClick("Rock")}> Rock</button>
-                        <button className="flex items-center gap-[5px] justify-center h-[29px] min-w-[64px] text-xs leading-6 border border-solid border-black rounded-[9px] bg-transparent transition-all ease-linear duration-200 font-normal text-[#14191C]" onClick={() => handleCategoryClick("More")}> More <ArrowIcon /></button>
+                        <button className={`min-w-[43px] h-[29px] text-xs leading-6 border border-solid border-black rounded-[9px] transition-all ease-linear duration-200 font-normal text-[#14191C] ${musicCategory === "All" ? "bg-black text-white" : "bg-white"}`}  onClick={() => handleCategoryClick("All")}> All</button>
+                        <button className={`min-w-[47px] h-[29px] text-xs leading-6 border border-solid border-black rounded-[9px] transition-all ease-linear duration-200 font-normal text-[#14191C] ${musicCategory === "Pop" ? "bg-black text-white" : "bg-white"}`} onClick={() => handleCategoryClick("Pop")}>Pop</button>
+                        <button className={`h-[29px] min-w-[54px] text-xs leading-6 border border-solid border-black rounded-[9px] transition-all ease-linear duration-200 font-normal text-[#14191C] ${musicCategory === "Rock" ? "bg-black text-white" : "bg-white"}`} onClick={() => handleCategoryClick("Rock")}>Rock</button>
+                        <button className={`flex items-center gap-[5px] justify-center h-[29px] min-w-[64px] text-xs leading-6 border border-solid border-black rounded-[9px] transition-all ease-linear duration-200  font-normal text-[#14191C] ${musicCategory === "More" ? "bg-black text-white button-svg" : "bg-white "}`} onClick={() => handleCategoryClick("More")}>More <ArrowIcon /></button>
                     </div>
                     {ALPHABET_LIST.map((obj, i) => (
                         <p className="size-[29px] max-xl:overflow-x-scroll min-w-[29px] text-xs hover:text-[#FAFAFF] cursor-pointer font-medium leading-[18px] rounded-full flex items-center justify-center bg-transparent transition-all ease-linear duration-200 hover:bg-black text-[#14191C]" key={i} onClick={() => handleAlphabetChange(obj)}> {obj}</p>
@@ -41,7 +43,7 @@ const Hero = () => {
                 </div>
                 <div className="w-full max-w-[1141px] mx-auto rounded-[20px] bg-[#14191C] h-[347px] relative mt-[43px] max-lg:h-full max-lg:mt-7">
                     <div className="flex justify-between max-lg:flex-wrap">
-                        <div className="flex flex-col lg:ps-[48px] pt-[38px] max-sm:px-3 max-xl:pt-8 max-md:pt-5 max-xl:w-full max-lg:items-center max-lg:justify-center">
+                        <div className="flex flex-col lg:ps-[48px] pt-[38px] max-sm:px-4 max-xl:pt-8 max-md:pt-5 max-xl:w-full max-lg:items-center max-lg:justify-center">
                             <h1 className="text-5xl font-montserrat max-lg:text-4xl max-md:text-3xl max-sm:text-2xl font-bold leading-[58.51px] max-md:text-center text-[#FAFAFF] uppercase">{getHeading()}</h1>
                             <div className="flex items-center gap-6 absolute xl:bottom-[-58px] bottom-[-44px] max-lg:bottom-0 max-lg:hidden">
                                 <img src="./assets/images/album-dp.webp" alt="user-img" className="cursor-pointer max-xl:size-40" />
@@ -58,7 +60,7 @@ const Hero = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="py-[43px] pe-[43px] max-lg:p-10 max-md:p-6 max-sm:p-4 max-lg:w-full">
+                        <div className="py-[43px] pe-[43px] max-lg:p-8 max-md:p-6 max-sm:p-5 max-lg:w-full">
                             <img src="./assets/images/singer-img.webp" alt="billie-eillish" className=" max-md:mx-auto w-full lg:min-w-[261px] max-lg:max-w-[261px] max-lg:mx-auto" />
                         </div>
                     </div>
